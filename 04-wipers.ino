abbc81c0c4ad7ebac5dbfe679e7c6096291e0b5d
othmar52 <othmar52@users.noreply.github.com>
2022-01-17 12:17:33 +0100

    [TASK] increased wiper intervals to 10 steps
    this is only temporary for for real life testing purposes

--- 04-wipers.ino
+++ 04-wipers.ino
@@ -7,6 +7,41 @@ void setupDigiPot() {
 
 void loopWiperModes() {
   currentWiperMode++;
+  if(currentWiperMode > WIPER_MODE_TMP10) {
+    currentWiperMode = WIPER_MODE_OFF;
+  }
+  Serial.print("current wiper mode is now ");
+  Serial.println(currentWiperMode);
+
+  // set resistance of digital potentiometer X9C
+  // https://www.digikey.at/de/products/detail/renesas-electronics-america-inc/X9C102PIZ/1068170
+  // thanks to https://www.youtube.com/watch?v=KqBruMXQjmA
+
+  // TODO check which values to set
+  // digiPot.setPotMin(false); ->   33 Ohm
+  // digiPot.setPot(18, false); -> 203 Ohm
+  // digiPot.setPot(20, false); -> 220 Ohm
+  // digiPot.setPot(25, false); -> 268 Ohm
+  // digiPot.setPot(50, false); -> 503 Ohm
+  // digiPot.setPot(75, false); -> 740 Ohm
+  // digiPot.setPotMax(false); ->  960 Ohm
+
+  if (currentWiperMode == WIPER_MODE_OFF)  {   digiPot.setPot(0, false); }
+  if (currentWiperMode == WIPER_MODE_TMP1) {   digiPot.setPotMin(false); }
+  if (currentWiperMode == WIPER_MODE_TMP2) {   digiPot.setPot(5, false); }
+  if (currentWiperMode == WIPER_MODE_TMP3) {   digiPot.setPot(10, false); }
+  if (currentWiperMode == WIPER_MODE_TMP4) {   digiPot.setPot(15, false); }
+  if (currentWiperMode == WIPER_MODE_TMP5) {   digiPot.setPot(18, false); }
+  if (currentWiperMode == WIPER_MODE_TMP6) {   digiPot.setPot(25, false); }
+  if (currentWiperMode == WIPER_MODE_TMP7) {   digiPot.setPot(40, false); }
+  if (currentWiperMode == WIPER_MODE_TMP8) {   digiPot.setPot(60, false); }
+  if (currentWiperMode == WIPER_MODE_TMP9) {   digiPot.setPot(80, false); }
+  if (currentWiperMode == WIPER_MODE_TMP10){   digiPot.setPotMax(false); }
+}
+
+
+void loopWiperModesBACKUP() {
+  currentWiperMode++;
   if(currentWiperMode > WIPER_MODE_FAST) {
     currentWiperMode = WIPER_MODE_OFF;
   }
